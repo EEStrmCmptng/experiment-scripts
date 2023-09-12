8886ecf09670aee624e09ba3e344b81b2bbd055c
hand32 <handong@bu.edu>
2023-09-12 15:50:51 -0600

    update ports

--- cloudlab_setup/c6220/set_iptables.sh
+++ cloudlab_setup/c6220/set_iptables.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# list current rules
-sudo iptables --list-rules
-
 # set default to accept everything
 sudo iptables -P INPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
@@ -27,8 +24,27 @@ sudo iptables -A INPUT -s 10.10.1.1 -j ACCEPT
 sudo iptables -A INPUT -s 10.10.1.2 -j ACCEPT
 sudo iptables -A INPUT -s 10.10.1.3 -j ACCEPT
 
+# allow all incoming ssh
+sudo iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# allow outgoing ssh
+sudo iptables -A OUTPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# allow all incoming http, https
+sudo iptables -A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+#flink ports
+sudo iptables -A INPUT -p tcp -m multiport --dports 6123,8081 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -m multiport --dports 6123,8081 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
 # set all packets not matching these rules to drop
 sudo iptables -P INPUT DROP                                            
 
 # disable packet forward/routing
 sudo iptables -P FORWARD DROP
+
+# list current rules
+sudo iptables --list-rules
