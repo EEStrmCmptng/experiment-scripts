23ba4422bde818c5993dd252ea9726da30776e7c
handong32 <handong@bu.edu>
2023-10-04 12:18:00 -0600

    update setup.sh

--- cloudlab_setup/c6220/setup.sh
+++ cloudlab_setup/c6220/setup.sh
@@ -9,7 +9,7 @@ echo off | sudo tee /sys/devices/system/cpu/smt/control
 # disable TurboBoost
 echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 
-# enable MSR
+# enable MSR to set DVFS statically
 sudo modprobe msr
 # lets run without sudo
 sudo setcap cap_sys_rawio=ep /usr/sbin/rdmsr 
@@ -21,6 +21,6 @@ pip install numpy pandas flink-rest-client
 # disable irq rebalance
 sudo killall irqbalance
 
-# set irq affinity
+# set irq affinity - make sure receive/transmit queues are mapped to the same core
 ieth=$(ifconfig | grep -B1 10.10.1 | grep -o "^\w*")
 sudo ./intel_set_irq_affinity.sh -x all ${ieth}
