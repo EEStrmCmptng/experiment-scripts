ccd3eab2a0adb8ff9e283fb65ce11efb58aeb2ab
Han <handong@bu.edu>
2023-10-23 17:26:14 +0000

    update ufw

--- cloudlab_setup/c6220/set_ufw.sh
+++ cloudlab_setup/c6220/set_ufw.sh
@@ -11,6 +11,7 @@ sudo ufw allow ssh
 sudo ufw allow from 10.10.1.1
 sudo ufw allow from 10.10.1.2
 sudo ufw allow from 10.10.1.3
+sudo ufw allow from 10.10.1.4
 
 # clear current ports just in case
 sudo ufw delete allow 8081
@@ -22,19 +23,23 @@ sudo ufw delete allow 443
 sudo ufw allow from 10.10.1.1 to any port 8081 proto tcp
 sudo ufw allow from 10.10.1.2 to any port 8081 proto tcp
 sudo ufw allow from 10.10.1.3 to any port 8081 proto tcp
+sudo ufw allow from 10.10.1.4 to any port 8081 proto tcp
 
 sudo ufw allow from 10.10.1.1 to any port 6123 proto tcp
 sudo ufw allow from 10.10.1.2 to any port 6123 proto tcp
 sudo ufw allow from 10.10.1.3 to any port 6123 proto tcp
+sudo ufw allow from 10.10.1.4 to any port 6123 proto tcp
 
 sudo ufw allow from 10.10.1.1 to any port 80 proto tcp
 sudo ufw allow from 10.10.1.2 to any port 80 proto tcp
 sudo ufw allow from 10.10.1.3 to any port 80 proto tcp
+sudo ufw allow from 10.10.1.4 to any port 80 proto tcp
 
 sudo ufw allow from 10.10.1.1 to any port 443 proto tcp
 sudo ufw allow from 10.10.1.2 to any port 443 proto tcp
 sudo ufw allow from 10.10.1.3 to any port 443 proto tcp
- 
+sudo ufw allow from 10.10.1.4 to any port 443 proto tcp
+
 # deny everything else
 sudo ufw default allow outgoing
 sudo ufw default deny incoming
