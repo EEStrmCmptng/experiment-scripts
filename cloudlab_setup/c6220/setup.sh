948de6efc90667b97b3feed6b3bb1c414cc2495d
hand32 <hand32@node0.hand32-187382.bayopsys-pg0.apt.emulab.net>
2024-01-22 23:15:10 +0000

    unify setup.sh

--- cloudlab_setup/c6220/setup.sh
+++ cloudlab_setup/c6220/setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y cpufrequtils msr-tools maven openjdk-11-jdk-headless python3-pip
+set -x
 
-# disable HT
+APTCLONEDIR=~/cloudlab_c6220_JobManager_Ubuntu22_5.15.0-69-generic_10_18_2023.apt-clone.tar.apt-clone.tar.gz
+
+sudo apt-get update
+sudo apt-get install apt-clone
+
+# reloading system libraries needed
+sudo apt-clone restore $APTCLONEDIR
+
+# disable HyperThreads
 echo off | sudo tee /sys/devices/system/cpu/smt/control
 
 # disable TurboBoost
@@ -16,7 +23,7 @@ sudo setcap cap_sys_rawio=ep /usr/sbin/rdmsr
 sudo setcap cap_sys_rawio=ep /usr/sbin/wrmsr
 sudo setcap cap_net_admin+ep /usr/sbin/ethtool
 
-# flink related
+# flink related python libraries
 pip install numpy pandas flink-rest-client
 
 # disable irq rebalance
@@ -24,7 +31,50 @@ sudo killall irqbalance
 
 # set irq affinity - make sure receive/transmit queues are mapped to the same core
 ieth=$(ifconfig | grep -B1 10.10.1 | grep -o "^\w*")
-sudo ./intel_set_irq_affinity.sh -x all ${ieth}
+sudo ~/experiment-scripts/cloudlab_setup/c6220/intel_set_irq_affinity.sh -x all ${ieth}
 
-# this is causing firmware issues, disable for now
+# sets hostname depending on IP
+case $ieth in
+
+    "10.10.1.1")
+	sudo hostname JobManager10-1
+	;;
+
+    "10.10.1.2")
+	sudo hostname Source10-2
+	;;
+
+    "10.10.1.3")
+	sudo hostname Mapper10-3
+	;;
+
+    "10.10.1.4")
+	sudo hostname Sink10-4
+	;;
+
+    *)
+	echo -n "Unknown IP: ${ieth}"
+	;;
+esac
+
+# this is causing firmware issues on c6220 nodes, disable for now
 sudo rmmod mlx4_ib
+sudo rmmod mlx4_core
+
+# create /data for raplog service to run
+sudo mkdir /data
+cd ~/experiment-scripts/cloudlab_setup/c6220/uarch-configure/rapl-read/ && make raplog && sudo setcap cap_sys_rawio=ep raplog
+cd /etc/systemd/system && sudo ln -s ~/experiment-scripts/cloudlab_setup/c6220/rapl_service/rapl_log.service rapl_log.service
+
+# creates msr group and lets user rdmsr, wrmsr without sudo
+sudo groupadd msr
+sudo chgrp msr /dev/cpu/*/msr
+sudo ls -l /dev/cpu/*/msr
+sudo chmod g+rw /dev/cpu/*/msr
+sudo usermod -aG msr $(whoami)
+
+echo "**** NOTE: Re-login to this node for msr group changes to take effect ****"
+sudo newgrp msr
+
+
+
