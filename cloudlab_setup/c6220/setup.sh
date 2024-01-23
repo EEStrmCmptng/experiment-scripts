4fb2e9abf5d1b07bd2f49b412842dc0d9df530f6
hand32 <handong@bu.edu>
2024-01-23 01:13:50 +0000

    updates to fix some broken stuff

--- cloudlab_setup/c6220/setup.sh
+++ cloudlab_setup/c6220/setup.sh
@@ -3,6 +3,7 @@
 set -x
 
 APTCLONEDIR=~/cloudlab_c6220_JobManager_Ubuntu22_5.15.0-69-generic_10_18_2023.apt-clone.tar.apt-clone.tar.gz
+WORKDIR=~/experiment-scripts/cloudlab_setup/c6220
 
 sudo apt-get update
 sudo apt-get install apt-clone
@@ -24,17 +25,18 @@ sudo setcap cap_sys_rawio=ep /usr/sbin/wrmsr
 sudo setcap cap_net_admin+ep /usr/sbin/ethtool
 
 # flink related python libraries
-pip install -r requirements.txt
+pip install -r $WORKDIR/requirements.txt
 
 # disable irq rebalance
 sudo killall irqbalance
 
 # set irq affinity - make sure receive/transmit queues are mapped to the same core
 ieth=$(ifconfig | grep -B1 10.10.1 | grep -o "^\w*")
-sudo ~/experiment-scripts/cloudlab_setup/c6220/intel_set_irq_affinity.sh -x all ${ieth}
+sudo $WORKDIR/intel_set_irq_affinity.sh -x all ${ieth}
 
 # sets hostname depending on IP
-case $ieth in
+mip=$(ifconfig | grep -B1 10.10.1 | grep inet | grep -oP 'inet \K(\d+\.\d+\.\d+\.\d+)')
+case $mip in
 
     "10.10.1.1")
 	sudo hostname JobManager10-1
@@ -63,8 +65,66 @@ sudo rmmod mlx4_core
 
 # create /data for raplog service to run
 sudo mkdir /data
-cd ~/experiment-scripts/cloudlab_setup/c6220/uarch-configure/rapl-read/ && make raplog && sudo setcap cap_sys_rawio=ep raplog
-cd /etc/systemd/system && sudo ln -s ~/experiment-scripts/cloudlab_setup/c6220/rapl_service/rapl_log.service rapl_log.service
+cd $WORKDIR/uarch-configure/rapl-read/ && make raplog && sudo setcap cap_sys_rawio=ep raplog
+cd /etc/systemd/system && sudo ln -s $WORKDIR/rapl_service/rapl_log.service rapl_log.service
+
+# list current status
+sudo ufw status
+
+# setup firewall
+# allow ssh
+sudo ufw allow ssh
+
+# allow connections from the following IP
+sudo ufw allow from 10.10.1.1
+sudo ufw allow from 10.10.1.2
+sudo ufw allow from 10.10.1.3
+sudo ufw allow from 10.10.1.4
+
+# clear current ports just in case
+sudo ufw delete allow 11211
+sudo ufw delete allow 8081
+sudo ufw delete allow 6123
+sudo ufw delete allow 80
+sudo ufw delete allow 443
+
+# mcd port
+sudo ufw allow from 10.10.1.1 to any port 11211 proto tcp
+sudo ufw allow from 10.10.1.2 to any port 11211 proto tcp
+sudo ufw allow from 10.10.1.3 to any port 11211 proto tcp
+sudo ufw allow from 10.10.1.4 to any port 11211 proto tcp
+
+# only allow our testing nodes IP to use Flink ports
+sudo ufw allow from 10.10.1.1 to any port 8081 proto tcp
+sudo ufw allow from 10.10.1.2 to any port 8081 proto tcp
+sudo ufw allow from 10.10.1.3 to any port 8081 proto tcp
+sudo ufw allow from 10.10.1.4 to any port 8081 proto tcp
+
+sudo ufw allow from 10.10.1.1 to any port 6123 proto tcp
+sudo ufw allow from 10.10.1.2 to any port 6123 proto tcp
+sudo ufw allow from 10.10.1.3 to any port 6123 proto tcp
+sudo ufw allow from 10.10.1.4 to any port 6123 proto tcp
+
+sudo ufw allow from 10.10.1.1 to any port 80 proto tcp
+sudo ufw allow from 10.10.1.2 to any port 80 proto tcp
+sudo ufw allow from 10.10.1.3 to any port 80 proto tcp
+sudo ufw allow from 10.10.1.4 to any port 80 proto tcp
+
+sudo ufw allow from 10.10.1.1 to any port 443 proto tcp
+sudo ufw allow from 10.10.1.2 to any port 443 proto tcp
+sudo ufw allow from 10.10.1.3 to any port 443 proto tcp
+sudo ufw allow from 10.10.1.4 to any port 443 proto tcp
+
+# deny everything else
+sudo ufw default allow outgoing
+sudo ufw default deny incoming
+
+# enable ufw
+sudo ufw enable
+sudo ufw status
+
+# disable redundant logging messages
+sudo ufw logging off
 
 # creates msr group and lets user rdmsr, wrmsr without sudo
 sudo groupadd msr
