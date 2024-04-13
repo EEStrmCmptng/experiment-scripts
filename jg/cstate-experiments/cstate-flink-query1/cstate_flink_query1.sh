8b55dcb0c9c5b1b2fe46683e2c2162b0d8601763
Jake Gustin <jakegust2020@gmail.com>
2024-04-13 17:29:22 +0000

    rerun of cstate-first, cstate-flink-query1 initial results. Need to do another cstate-first run with time addition

--- jg/cstate-experiments/cstate-flink-query1/cstate_flink_query1.sh
+++ jg/cstate-experiments/cstate-flink-query1/cstate_flink_query1.sh
@@ -6,15 +6,15 @@ export IPMAPPER=${IPMAPPER:="10.10.1.3"}
 for ((state = 0; state < 2; state++)); do # 2 iterations: 0 = cstates enabled, 1 = cstates disabled
     echo Running with State $state.
     ~/experiment-scripts/changeSleep.sh $state
-    for ((rate=1000000; rate <= 8000000; rate+=1000000)); do #set flink rate to 1M, 2M, ..., 8M
+    for ((rate=100000; rate <= 600000; rate+=100000)); do #set flink rate to 100k, 200k, ..., 600k
         for policy in $POLICIES; do
-            SLEEPDISABLE=$state MPOLICY=$policy MQUERY=query1 MCFG="1;16;16" NSOURCES=1 NSINKS=16 NMAPPERS=16 FLINK_RATE="${rate}_300000" /users/gustinj/experiment-scripts/run_anyquery.sh
+            SLEEPDISABLE=$state MPOLICY=$policy MQUERY=query1 MCFG="16;16;16" NSOURCES=16 NSINKS=16 NMAPPERS=16 FLINK_RATE="${rate}_300000" /users/gustinj/experiment-scripts/run_query1.sh dynamic
 	done
     done
 done
 
 python parse_simplified.py --name query1 --log ~/experiment-scripts/logs
-cp ~/experiment-scripts/combined.csv ./results.csv
+cp ~/experiment-scripts/logs/combined.csv ./results.csv
 
 echo "ALL RUNS COMPLETE"
 
